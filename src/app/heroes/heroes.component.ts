1c294e1800c36e3e7223596bf548242494ddb39c
LucasDueck <lucas.dueck@weidmueller.com>
2022-02-09 08:32:01 +0000

    Sicherheitsstand Kapitel 2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-//import { Hero } from '../hero';
-//import { HEROES } from '../mock-heroes';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,9 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  //heroes = HEROES;
+  heroes = HEROES;
 
- // hero: Hero = {    id: 1,  name: 'Windstorm'};
+  hero: Hero = {    id: 1,  name: 'Windstorm'};
 
   constructor() { }
 
